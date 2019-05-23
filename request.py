7140c9b776ee980982a317ca95736050b82a328c
Lucas <lucasdevpython@gmail.com>
2019-05-23 00:09:51 -0300

    Segundo commit, listando letra por letra do dict, próximo passo trocar as letras.

--- request.py
+++ request.py
@@ -4,8 +4,11 @@ import urllib.request as ur
 f = ur.urlopen("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=d8a2c7d19ed638555dae59b9ef594f1dc5378e27")
 s1 = f.read()
 
-print(s1)
+print(str(s1))
 
+json = open('answer.json', 'wb')
+json.write(s1)
+json.close()
 
 #TOKEN = d8a2c7d19ed638555dae59b9ef594f1dc5378e27
 
---
+++ teste_decript.py
@@ -0,0 +1,22 @@
+dic = {"cifrado":"c okurncegf fgekocn rqkpv yknn cnycau gpf wr yjgtg kv yknn fq vjg itgcvguv fcocig. wpmpqyp"}
+dic2 = dic['cifrado']
+dic3 = []
+#Devo contar a letra menos 10
+
+#abc = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')
+#        1   2   3   4   5   6   7   8   9   10  11  12  13  14  15  16  17  18  19  20  21  22  23  24  25  26
+#        17  18  19  20  21  22  23  24  25  25  1    2   3   4   5   6   7   8   9  10  11  12  13  14  15  16
+print(len(dic2))
+for l in dic2:
+    if l =='a':
+        dic3.append('k')
+    if l =='b':
+        dic3.append('j')
+    if l =='c':
+        dic3.append()
+
+
+print(len(abc))
+
+
+print(dic2)
