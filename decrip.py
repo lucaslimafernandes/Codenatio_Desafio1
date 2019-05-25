a8944843ca965a5559ce9319efbfd852ac1a269a
Lucas <lucasdevpython@gmail.com>
2019-05-24 23:13:55 -0300

    Primeiro funcional de decifrar

---
+++ decrip.py
@@ -0,0 +1,18 @@
+carac = 'abcdefghijklmnopqrstuvwxyz'
+chave = 26 + 2
+
+s1 = ('c okurncegf fgekocn rqkpv yknn cnycau gpf wr yjgtg kv yknn fq vjg itgcvguv fcocig. wpmpqyp')
+conv = ''
+#def decriptar(s1):
+
+for c in s1:
+    num = carac.find(c)
+    num2 = num - chave
+    if num2 >= len(carac):
+        num2 -= len(carac)
+    elif num2 < 0:
+        num2 += len(carac)
+        conv += carac[num2]
+    else:
+        conv += c
+print(conv)
